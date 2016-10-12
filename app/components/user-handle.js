5dfda0229d2e731852df4ae34d3b8021e8e52e3e
Pascal Boutin <pboutin@webcliniq.com>
2016-10-11 23:53:10 -0400

    moved the logout in the proper component

--- app/components/user-handle.js
+++ app/components/user-handle.js
@@ -3,6 +3,14 @@ import Ember from 'ember';
 export default Ember.Component.extend({
     session: null,
 
+    actions: {
+        signOut: function() {
+            this.get('session').close().then(function() {
+                window.location.reload();
+            });
+        }
+    },
+
     isGoogle: Ember.computed('session.provider', function() {
         return this.get('session.provider') === 'google.com';
     })
