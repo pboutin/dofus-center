da6670bb396be0fe47cf0428e6125fa4dad07b20
Pascal Boutin <pboutin@webcliniq.com>
2016-12-28 15:58:05 -0500

    Refactor i18n for nested templates

--- app/locales/fr/translations.js
+++ app/locales/fr/translations.js
@@ -11,40 +11,45 @@ export default {
         'login_with_facebook': "Se connecter avec Facebook",
         'login_with_google': "Se connecter avec Google"
     },
-    'projects': {
-        'new': "Créer un nouveau projet",
-        'public_link': "Lien publique",
-        'save': "Sauvegarder",
-        'cancel': "Cancel",
-        'name': "Nom du projet",
-        'close': "Fermer",
-        'prepare': "Modifier",
-        'go_to_workbench': "Passer à l'atelier",
-        'delete': "Supprimer",
-        'delete_confirmation': "Êtes-vous sûr de vouloir supprimer le projet"
+    'dashboard': {
+        'workbench': "Atelier d'artisanat",
+        'almanax': "Almanax"
     },
-    'prepare': {
-        'manage': "Gestion du projet",
-        'search': "Ajouter un item...",
-        'empty_project': "Votre projet est présentement vide",
-        'back_to_projects': "Retourner à la liste des projets",
-        'go_to_workbench': "Passer à l'atelier",
-    },
-    'crafting': {
-        'workbench_for': "Atelier pour",
-        'edit_project': "Modifier le projet",
-        'go_to_projects': "Retourner à la liste des projets",
-        'your_project': "Votre projet",
-        'send_to_workbench': "Envoyer à l'atelier",
-        'hide_completed': "Cacher les ressources complétées",
-        'show_everything': "Afficher tout les ressources",
-        'empty_project': "Votre projet est présentement vide",
-        'completed_project': "Votre projet semble complété !"
-    },
-    'view': {
-        'project': "Projet",
-        'go_to_projects': "Aller à la liste des projets",
-        'login': "Se connecter",
-        'empty_project': "Ce projet est présentement vide",
+    'workbench': {
+        'projects': {
+            'new': "Créer un nouveau projet",
+            'public_link': "Lien publique",
+            'save': "Sauvegarder",
+            'cancel': "Cancel",
+            'name': "Nom du projet",
+            'close': "Fermer",
+            'prepare': "Modifier",
+            'go_to_workbench': "Passer à l'atelier",
+            'delete': "Supprimer",
+            'delete_confirmation': "Êtes-vous sûr de vouloir supprimer le projet"
+        },
+        'prepare': {
+            'manage': "Gestion du projet",
+            'search': "Ajouter un item...",
+            'back_to_projects': "Retourner à la liste des projets",
+            'go_to_workbench': "Passer à l'atelier",
+        },
+        'crafting': {
+            'workbench_for': "Atelier pour",
+            'edit_project': "Modifier le projet",
+            'go_to_projects': "Retourner à la liste des projets",
+            'your_project': "Votre projet",
+            'send_to_workbench': "Envoyer à l'atelier",
+            'hide_completed': "Cacher les ressources complétées",
+            'show_everything': "Afficher tout les ressources",
+            'empty_project': "Votre projet est présentement vide",
+            'completed_project': "Votre projet semble complété !"
+        },
+        'view': {
+            'project': "Projet",
+            'go_to_projects': "Aller à la liste des projets",
+            'login': "Se connecter",
+            'empty_project': "Ce projet est présentement vide",
+        }
     }
 };
